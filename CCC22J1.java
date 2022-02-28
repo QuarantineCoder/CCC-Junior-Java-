b69d5429c1c7148d90e6254c056bb553af1cc042
Sahil Panchal <100591536+QuarantineCoder@users.noreply.github.com>
2022-02-28 09:32:52 -0500

    CCC '21 J1 -Cupcake Party

---
+++ CCC22J1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int r = sc.nextInt() * 8;
+		int s = sc.nextInt() * 3;
+		sc.close();
+
+		System.out.println(r + s - 28);
+
+	}
+
+}
