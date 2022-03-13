e66564750e994546573b2d1aaa81f638ab2075c7
Sahil Panchal <100591536+QuarantineCoder@users.noreply.github.com>
2022-03-12 23:11:25 -0500

    Added commenting

--- 2018/Junior/CCC18J3.java
+++ 2018/Junior/CCC18J3.java
@@ -6,16 +6,19 @@ public class TestinLand {
 
         Scanner sc = new Scanner(System.in);
         
+        //Takes integer values for the 4 different cities
         int c1 = sc.nextInt();
         int c2 = sc.nextInt();
         int c3 = sc.nextInt();
         int c4 = sc.nextInt();
         sc.close();
         
+        //Finds distance between cities
         int c1c2 = c1 + c2;
         int c1c2c3 = c1 + c2 + c3;
         int c1c2c3c4 = c1 + c2 + c3 + c4;
         
+        //Prints out the different distances between cities
         System.out.println("0 " + c1 + " " + c1c2 + " " + c1c2c3 + " " + c1c2c3c4);
         System.out.println(c1 + " " + (c1 - c1) + " " + (c1c2 - c1) + " " + (c1c2c3 - c1) + " " + (c1c2c3c4 - c1));
         System.out.println(c1c2 + " " + (Math.abs(c1 - c1c2)) + " " + (Math.abs(c1c2 - c1c2)) + " " + (Math.abs(c1c2c3 - c1c2)) + " " + (Math.abs(c1c2c3c4 - c1c2)));
