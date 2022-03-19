e6c730cd3dd761ea526d028efa643cd63320439a
Sahil Panchal <100591536+QuarantineCoder@users.noreply.github.com>
2022-03-19 00:19:47 -0400

    Create CCC07J2.java

---
+++ 2007/Junior/CCC07J2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class TestinLand {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.next();
+		
+		while(!input.contains("TTYL")) {
+			if(input.contains("CU")) 
+				System.out.println("see you");
+			else if(input.contains(":-)"))
+				System.out.println("I'm happy");
+			else if (input.contains(":-("))
+				System.out.println("I'm unhappy");
+			else if(input.contains(";-)"))
+				System.out.println("wink");
+			else if (input.contains(":-P"))
+				System.out.println("stick out my tongue");
+			else if(input.contains("(~.~)"))
+				System.out.println("sleepy");
+			else if (input.contains("TA"))
+				System.out.println("totally awesome");
+			else if(input.contains("CCC"))
+				System.out.println("Canadian Computing Competition");
+			else if (input.contains("CUZ"))
+				System.out.println("because");
+			else if(input.contains("TY"))
+				System.out.println("thank-you");
+			else if (input.contains("YW"))
+				System.out.println("you're welcome");
+			else System.out.println(input);
+			
+			input = sc.next();
+		}sc.close();
+		
+		System.out.println("talk to you later");
+	}
+}
